75258705da31bf9d8c79f6bc4081834bad719a35
Ognjen Vračević <ovracevic@smart.edu.rs>
2021-06-09 15:53:22 +0200

    Day 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KomponentaComponent } from './komponenta/komponenta.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetialComponent } from './hero-detial/hero-detial.component';
+import { UserPostsComponent } from './user-posts/user-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KomponentaComponent,
+    HeroesComponent,
+    HeroDetialComponent,
+    UserPostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
---
+++ src/app/hero-detial/hero-detial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detial',
+  templateUrl: './hero-detial.component.html',
+  styleUrls: ['./hero-detial.component.css']
+})
+export class HeroDetialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
